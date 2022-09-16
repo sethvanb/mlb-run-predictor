d8114a3213447195170b3e251d94032b7dc637a0
Seth VanBrocklin <seth.vanbrocklin@appfolio.com>
2022-09-15 20:48:31 -0700

    my commit message

--- server/index.js
+++ server/index.js
@@ -11,38 +11,42 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.use(cors());
 
-app.get("/api/predict", async (req, res) => {
-  var options = {
-    mode: 'text',
-    pythonPath: 'python3',
-    pythonOptions: ['-u'],
-    scriptPath: './server/',
-    args: [req.query.homeTeam, req.query.awayTeam]
-  };
+// app.get("/api/predict", async (req, res) => {
+//   var options = {
+//     mode: 'text',
+//     pythonPath: 'python3',
+//     pythonOptions: ['-u'],
+//     scriptPath: './server/',
+//     args: [req.query.homeTeam, req.query.awayTeam]
+//   };
 
-  PythonShell.run('inference.py', options, function (err, results) {
-    if (err) 
-      throw err;
-    console.log(results);
-    res.json({ results: results });
-  });
-});
+//   PythonShell.run('inference.py', options, function (err, results) {
+//     if (err) 
+//       throw err;
+//     console.log(results);
+//     res.json({ results: results });
+//   });
+// });
 
-app.get("/api/games", async (req, res) => {
-  var options = {
-    mode: 'text',
-    pythonPath: 'python3',
-    pythonOptions: ['-u'],
-    scriptPath: './server/',
-    args: []
-  };
+// app.get("/api/games", async (req, res) => {
+//   var options = {
+//     mode: 'text',
+//     pythonPath: 'python3',
+//     pythonOptions: ['-u'],
+//     scriptPath: './server/',
+//     args: []
+//   };
 
-  PythonShell.run('dailyGames.py', options, function (err, results) {
-    if (err) 
-      throw err;
-    console.log(results);
-    res.json(results);
-  });
+//   PythonShell.run('dailyGames.py', options, function (err, results) {
+//     if (err) 
+//       throw err;
+//     console.log(results);
+//     res.json(results);
+//   });
+// });
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
 });
 
 app.get('*', (req, res) => {
